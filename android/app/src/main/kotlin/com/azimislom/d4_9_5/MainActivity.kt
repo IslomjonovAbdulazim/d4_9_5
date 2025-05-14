7eb35ffeff470c435834ee7156ee211918e21732
IslomjonovAbdulazim <111278113+IslomjonovAbdulazim@users.noreply.github.com>
2025-05-14 17:10:46 +0500

    Initial commit

---
+++ android/app/src/main/kotlin/com/azimislom/d4_9_5/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.azimislom.d4_9_5
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:d4_9_5/pages/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
+
